dc08973dfd4e7883345b9813149f2ca2e0701453
kryachko.2022 <kryachko.2022@CLOUD-RDHOST1>
2024-01-15 11:42:30 +0700

    Добавьте файлы проекта.

---
+++ 15.01/15.01.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include <iostream>
+#include <locale.h>
+
+void go_south_east(int *lat, int *lon)
+{
+	*lat = *lat - 1;
+	*lon = *lon + 1;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int latitude = 32;
+	int longitude = -64;
+		go_south_east (&latitude, &longitude);
+	printf("Стоп, теперь наши координаты: [%i, %i]\n", latitude, longitude);
+	return 0;
+	
+}
---
+++ 15.10 (2)/15.10 (2).cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	char masked_raider[] = "Живой!";
+	char* jimmy = masked_raider;
+	printf("Грабитель в маске сейча %s Джимми сейчас %s\n", masked_raider, jimmy);
+	masked_raider[0] = 'М';
+	masked_raider[1] = 'Е';
+	masked_raider[2] = 'Р';
+	masked_raider[3] = 'Т';
+	masked_raider[4] = 'В';
+	masked_raider[5] = '!';
+	printf("Грабитель в маске сейчас %s Джимми сейчас %s\n", masked_raider, jimmy);
+	return 0;
+}
+
